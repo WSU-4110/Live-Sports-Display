9ab3675db0cf07722f1d37f2f0ddb1db44f1dc98
Najib_Mohammed <gm2403@wayne.edu>
2024-03-05 22:58:02 -0500

    Image Preprocessing
    Updated Image Preprocessing Functions.

--- OCR-1/OCR-1.py
+++ OCR-1/OCR-1.py
@@ -2,8 +2,10 @@ import cv2
 from PIL import Image
 from matplotlib import pyplot as plt
 
-image_file = "Pictures/Sample.png"
-image = cv2.imread(image_file)
+teams1="Pictures/OCR Team Sample.png"
+teams1_image=cv2.imread(teams1)
+teams2="Pictures/OCR Team Sample-2.jpg"
+teams2_image=cv2.imread(teams2)
 
 
 # function to display the image.
@@ -18,42 +20,56 @@ def display_image(im_path):
     ax.imshow(image_data, cmap='gray')
     plt.show()
 
-
-# function to invert the image.
 def inverted_image(image):
     inverted_image = cv2.bitwise_not(image)
-    cv2.imwrite("Pictures/inverted.png", inverted_image)
-    display_image("Pictures/inverted.png")
-
-
+    cv2.imwrite("Pictures/Inverted Image.png", inverted_image)
+    inverted="Pictures/Inverted Image.png"
+    return inverted,inverted_image
 def grayscale(image):
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite("Pictures/gray_image.png", gray_image)
-    display_image("Pictures/gray_image.png")
+    cv2.imwrite("Pictures/Gray Image.png", gray_image)
+    gray="Pictures/Gray Image.png"
+    return gray,gray_image
+
 
 def black_and_white(image):
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    thresh,black_and_white=cv2.threshold(gray_image, 210, 230, cv2.THRESH_BINARY)
-    cv2.imwrite("Pictures/black_and_white.png", black_and_white)
-    display_image("Pictures/black_and_white.png")
-    return black_and_white
-
-def noise_removal(image):
-    black_and_white_image=black_and_white(image)
-    import numpy as np
-    kernel = np.ones((1, 1), np.uint8)
-    black_and_white_image = cv2.dilate(image, kernel, iterations=1)
-    kernel = np.ones((1, 1), np.uint8)
-    black_and_white_image = cv2.erode(image, kernel, iterations=1)
-    black_and_white_image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
-    black_and_white_image = cv2.medianBlur(image, 3)
-    noise_removed=cv2.imwrite("temp/no_noise.jpg", black_and_white_image)
-    display_image(noise_removed)
-    return (black_and_white_image)
+    gray,gray_image = grayscale(image)
+    thresh,black_and_white_image=cv2.threshold(gray_image, 210, 230, cv2.THRESH_BINARY)
+    cv2.imwrite("Pictures/Black and White.png", black_and_white_image)
+    black_and_white="Pictures/Black and White.png"
+    return black_and_white, black_and_white_image
 
 
-display_image(image_file)
-inverted_image(image)
-grayscale(image)
-black_and_white(image)
-noise_removal(image)
+def bounding_boxes(image):
+    gray,gray_image = grayscale(image)
+    blur=cv2.GaussianBlur(gray_image,(7,7),0)
+    thresh=cv2.threshold(blur,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)[1]
+    index_kernel_image=cv2.getStructuringElement(cv2.MORPH_RECT,(3,13))
+    dilate=cv2.dilate(thresh,index_kernel_image,iterations=1)
+    cv2.imwrite("Pictures/Index.png",index_kernel_image)
+    contours=cv2.findContours(dilate,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    contours=contours[0]\
+        if len(contours)==2 \
+        else contours[1]
+    contours=sorted(contours,key=lambda x: cv2.boundingRect(x)[0])
+    bounding_box_image = image
+    for c in contours:
+        x, y, w, h = cv2.boundingRect(c)
+        if (h < 30 and w > 36):
+            cv2.rectangle(bounding_box_image, (x, y), (x + w, y + h), (36, 255, 12), 2)
+    cv2.imwrite("Pictures/Bounding Boxes.png", bounding_box_image)
+    bounding_boxes = "Pictures/Bounding Boxes.png"
+    return bounding_boxes,bounding_box_image
+
+
+display_image(teams1)
+inverted,inverted_image=inverted_image(teams1_image)
+display_image(inverted)
+gray,gray_image=grayscale(teams1_image)
+display_image(gray)
+black_and_white, black_and_white_image=black_and_white(teams1_image)
+display_image(black_and_white)
+bounding_boxes,bounding_boxes_image=bounding_boxes(teams1_image)
+display_image(bounding_boxes)
+
+
