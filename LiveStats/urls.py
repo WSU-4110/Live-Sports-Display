e461610c19d8c940a35039813013a9a84479c91e
Jordan Grewe <114885319+Grewestr@users.noreply.github.com>
2024-03-19 18:18:36 -0400

    Update urls.py

--- LiveStats/urls.py
+++ LiveStats/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 #import django_toolbar
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
