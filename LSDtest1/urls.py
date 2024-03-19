915479eb31ac18b334e161f922b492518d7fc13f
Jordan Grewe <114885319+Grewestr@users.noreply.github.com>
2024-03-19 17:54:07 -0400

    Update urls.py
    heroku update

--- LSDtest1/urls.py
+++ LSDtest1/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('LiveStats/', include('LiveStats.urls')),
     path("__debug__/", include("debug_toolbar.urls")),
+    path('upload/', views.upload_and_ocr, name='upload'),
+    path('', views.home_view, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
