0b58d463dc98e56e91fa3d1281694c012a3e3fb6
Jordan Grewe <114885319+Grewestr@users.noreply.github.com>
2024-03-25 23:20:15 -0400

    Update settings.py

--- LSDtest1/settings.py
+++ LSDtest1/settings.py
@@ -28,7 +28,11 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-l-bv0-ty@p0gwk
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+CELERY_BROKER_URL = os.environ.get('REDIS_URL', 'redis://:p63b5491fe55772d47f3ca46afef7e6fef54361a4806fe5a092a9f996704eb238@ec2-3-215-140-221.compute-1.amazonaws.com:28649') 
+CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL', 'redis://:p63b5491fe55772d47f3ca46afef7e6fef54361a4806fe5a092a9f996704eb238@ec2-3-215-140-221.compute-1.amazonaws.com:28649')
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
 
 
 #ALLOWED_HOSTS = []
