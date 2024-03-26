5ba6b96ab399fc1a6ef8bf498e38d3b92dfde41d
Jordan Grewe <114885319+Grewestr@users.noreply.github.com>
2024-03-26 14:05:58 -0400

    Update tasks.py

--- LSDtest1/tasks.py
+++ LSDtest1/tasks.py
@@ -1,8 +1,11 @@
 # tasks.py
 
 from celery import shared_task
+import logging
 
+logger = logging.getLogger(__name__)
 @shared_task
 def my_background_task():
+    logger.info('Running the asynchronous task...')
     result  = 5
     return result
