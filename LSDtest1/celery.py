2fa5dd6bcabd7cbae0d0b99fe3320a15ce4ff84d
Jordan Grewe <114885319+Grewestr@users.noreply.github.com>
2024-03-25 23:42:50 -0400

    Update celery.py

--- LSDtest1/celery.py
+++ LSDtest1/celery.py
@@ -1,8 +1,18 @@
+from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from django.conf import settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LSDtest1.settings')
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
-app = Celery('LSDtest1')
+app = Celery('myproject')
+
+# Using a string here means the worker process will serialize
+# the configuration object to child processes.
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+app.conf.broker_url = os.environ.get('REDIS_URL', 'redis://localhost:6379/0')
