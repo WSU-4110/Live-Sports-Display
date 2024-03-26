1c471c29ed89393d78bdfae36a226efe0c0866f1
Jordan Grewe <114885319+Grewestr@users.noreply.github.com>
2024-03-25 20:15:25 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 #from jordan
 import os
 
-
+print (top)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
