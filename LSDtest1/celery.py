da9098d18b9f2bd55a4bfef5a50bcbd1eac611e0
Jordan Grewe <114885319+Grewestr@users.noreply.github.com>
2024-03-26 01:12:55 -0400

    Update celery.py

--- LSDtest1/celery.py
+++ LSDtest1/celery.py
@@ -5,4 +5,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LSDtest1.settings')
 
 app = Celery('LSDtest1')
 app.config_from_object('django.conf:settings', namespace='CELERY')
-#app.autodiscover_tasks()
+app.autodiscover_tasks()
