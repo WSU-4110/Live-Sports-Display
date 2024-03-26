761f712c9b6cad1be3ab5e1f163cd033f162847b
Jordan Grewe <114885319+Grewestr@users.noreply.github.com>
2024-03-26 14:03:52 -0400

    Update settings.py

--- LiveStats/settings.py
+++ LiveStats/settings.py
@@ -123,6 +123,22 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'celery': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
