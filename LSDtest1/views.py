134b1564a5374482db20bf409ae06f58a5dce7ff
Jordan Grewe <114885319+Grewestr@users.noreply.github.com>
2024-03-26 00:15:28 -0400

    Update views.py

--- LSDtest1/views.py
+++ LSDtest1/views.py
@@ -24,7 +24,7 @@ def run_ssh(request):
             if action == "stackedDisplay":
                 # Offload the long-running operation to a background task
                 # response_message = run_stacked_display()  # This needs to be offloaded
-                result = my_background_task.delay(2, 3)
+                result = my_background_task.delay()
                 print(result.get(timeout=100))
                 return JsonResponse({"message": "Task is running in the background"})
             else:
