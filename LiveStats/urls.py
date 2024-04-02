0d0cd9c4d2b90bcea06b8f92d5215dc4337b65ae
Jordan Grewe <114885319+Grewestr@users.noreply.github.com>
2024-04-01 21:45:46 -0400

    Update urls.py

--- LiveStats/urls.py
+++ LiveStats/urls.py
@@ -1,4 +1,4 @@
-'''from django.contrib import admin
+from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
