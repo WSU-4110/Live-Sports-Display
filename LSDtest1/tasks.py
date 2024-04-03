fb70e9fd2b720839bb531d78eae3e15845f23e08
Jordan Grewe <114885319+Grewestr@users.noreply.github.com>
2024-04-03 15:06:29 -0400

    Create tasks.py
    recovering from merge loss

---
+++ LSDtest1/tasks.py
@@ -0,0 +1,9 @@
+from celery import shared_task
+import logging
+
+logger = logging.getLogger(__name__)
+@shared_task
+def my_background_task():
+    logger.info('Running the asynchronous task...')
+    result  = 5
+    return result
