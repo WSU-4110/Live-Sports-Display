91e6bdfb82e51d2b0d06656009e796702297d334
Mitchell Brown <MitchellBrownNE@gmail.com>
2024-04-05 20:51:51 -0400

    added html page

--- LSDtest1/views.py
+++ LSDtest1/views.py
@@ -92,7 +92,12 @@ def Get_Team_Stats(request):
     API = SportsAPI()
     stats = API.get_live_team_stats(inputted_team_name)
 
-    return render(request, 'team_stats.html', {'stats': stats})
+    context = {
+        'stats': stats,
+        'inputted_team_name': inputted_team_name
+    }
+
+    return render(request, 'team_stats.html', context)
 
 def Get_Live_Stats(request):
     inputted_team_name = request.GET.get('inputted_team_name')
