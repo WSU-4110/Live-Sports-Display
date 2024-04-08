9fda67cb556b8c94889c4efc95ac7673909b585b
Jordan Grewe <114885319+Grewestr@users.noreply.github.com>
2024-04-08 16:56:23 -0400

    Update settings.py

--- LSDtest1/settings.py
+++ LSDtest1/settings.py
@@ -155,8 +155,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # New line to define the directory where collectstatic will collect static files for deployment. Heroku
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # New line to use whitenoise to serve compressed static files Heroku
+BASE_DIR = Path(__file__).resolve().parent.parent
 STATICFILES_DIRS = [
-    BASE_DIR BASE_DIR / 'LiveStats' / 'static',
+    BASE_DIR / 'LiveStats' / 'static',
 ]
 
 
