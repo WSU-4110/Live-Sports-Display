68ed554d6c82cf2d98aa06318d46b88e642ae0fe
Najib-Mohammed <gm2403@wayne.edu>
2024-04-09 19:17:07 -0400

    Added more Patterns for the Regex
    For the regex part I added more patterns in order to account for more combinations. This will help capture more names.

--- OCR-1/MainExecution.py
+++ OCR-1/MainExecution.py
@@ -2,13 +2,18 @@ import cv2
 import pytesseract
 import re
 from nba_api.stats.static import players
-
+"""
+The following Libraries are needed:
+OpenCV: pip install opencv-python
+pytesseract: pip install pytesseract
+re: Usually comes with Python.
+nba_api: pip install nba_api
+"""
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'
-image_file= "Pictures/Sample-1.png"
+image_file= "Pictures/Sample-6.jpg"
 teams_image=cv2.imread(image_file)
 text=pytesseract.image_to_string(teams_image)
-
 
 def fullnames():
     full_names = []
@@ -25,7 +30,11 @@ def abbreviatednames():
     return abbreviated_names
 
 def extract_possible_shortened_names(value):
-    possible_shortened_names = re.findall(r'\b[A-Z]\.\s?[A-Z][a-z]+[-A-Z][-a-zA-Z]+\b|\b[A-Z]\.\s?[A-Z][a-z]+\b|\b[A-Z]\.\s?[A-Z][a-z]+[A-Z][a-z]+\b', value)
+    formatted_possible_shortened_names = re.findall(r'\b[A-Z]\.\s?[A-Z][a-z]+[-A-Z][-a-zA-Z]+\b|\b[A-Z]\.\s?[A-Z][a-z]+\b|\b[A-Z]\.\s?[A-Z][a-z]+[A-Z][a-z]+\b', value)
+    unformatted_possible_shortened_names= re.findall(r'\b[A-Z]\.\s?[A-Z][a-z]+\b', value)
+    clarity_pattern=re.compile(r'\.(?=[^\s])')
+    clear_possible_shortened_names=[clarity_pattern.sub('. ', string) for string in unformatted_possible_shortened_names]
+    possible_shortened_names=formatted_possible_shortened_names+clear_possible_shortened_names
     return possible_shortened_names
 
 def extract_possible_full_names(value):
@@ -39,12 +48,11 @@ def find_name_matches(value):
     extracted_full_names=list(set(fullnames()).intersection(possible_full_names))
     extracted_non_full_names=list(set(abbreviatednames()).intersection(shortened_names))
     locations=list(map(lambda x: abbreviatednames().index(x),extracted_non_full_names))
-    print(str(locations))
     complete_names=[]
     for i in range(len(locations)):
         complete_names.append(full_names[locations[i]])
     all_names=extracted_full_names+complete_names
     return all_names
 
-
 print("ALL: ",find_name_matches(text))
+print(len(find_name_matches(text)))
