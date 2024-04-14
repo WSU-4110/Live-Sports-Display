c3f3fc1e349478fe359191dea5416fe19aed63e8
Jordan Grewe <114885319+Grewestr@users.noreply.github.com>
2024-04-14 19:21:17 -0400

    Update views.py

--- LSDtest1/views.py
+++ LSDtest1/views.py
@@ -62,7 +62,7 @@ def run_single_display(request):
         hostname = 'us2.pitunnel.com'
         username = 'jordan'
         password = 'CSC4110LSD'
-        port = 44473
+        port = 55945
         try:
             # Initialize the SSH client with settings for PiTunnel access
             client = paramiko.SSHClient()
