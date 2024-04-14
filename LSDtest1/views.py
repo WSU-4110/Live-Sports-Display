5182b79a0d3a77174a8196eb093f15d538aafe23
Timothy M Kosinski <127711040+TKosinski24@users.noreply.github.com>
2024-04-14 13:38:58 -0400

    Update views.py

--- LSDtest1/views.py
+++ LSDtest1/views.py
@@ -24,7 +24,7 @@ def run_stacked_display(request):
         hostname = 'us2.pitunnel.com'  
         username = 'timkosinski'  
         password = '20010972'  
-        port = 36737  #Will change every bootup!
+        port = 51095  #Will change every bootup!
 
         execute_command = (
             f"echo '{player_names}' > /home/timkosinski/rpi-rgb-led-matrix/bindings/python/samples/Players.txt "
