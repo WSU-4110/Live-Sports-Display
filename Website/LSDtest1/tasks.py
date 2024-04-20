39526db2dbf50b0aca0c5e859296982eed8f9f80
Rory Jolliff <156139892+Nobody96103@users.noreply.github.com>
2024-04-19 21:29:59 -0400

    Assignment 6
    Deleting parts that are not tested

--- Website/LSDtest1/tasks.py
+++
@@ -1,7 +0,0 @@
-from celery import shared_task
-from .SSH import run_stacked_display  # Ensure this import is correct
-
-@shared_task
-def my_background_task():
-    run_stacked_display()
-    return "Completed"
